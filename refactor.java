aeef14f97eb1295de4c216fab8cee9ed0e952245
unknown <C06979A@W5101730.ena.us.experian.local>
2015-06-30 16:57:53 -0600

    added new file

---
+++ refactor.java
@@ -0,0 +1,14 @@
+public boolean function1(Person person, boolean isTrue)
+{
+     
+    String String1 = getName(); 
+
+	if (string1 != null && string1.equals("Nombre")){
+        isTrue = true;
+    }else{
+        isTrue = false;
+    }
+    
+	return isTrue;
+
+}
